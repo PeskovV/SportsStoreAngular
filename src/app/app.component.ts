76d628ec39c829701cd526612c21f355ab5f931b
Владислав <alladusheck@gmail.com>
2019-10-24 09:47:30 +0300

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: '<div class="bg-success m-1 p-1 text-center">This is SportsStore</div>'
+})
+export class AppComponent {
+  
+}
