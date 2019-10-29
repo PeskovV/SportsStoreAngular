5aee9f5928d2079aeb1a3b8bb662c9e884e4234d
Владислав <alladusheck@gmail.com>
2019-10-29 17:36:24 +0300

    Add cart and button "add to cart"

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.modules";
 import { StoreComponent } from "./store.component";
 import { CounterDirective } from './counter.directive';
+import { CartSummaryComponent } from './cartSummary.components';
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent, CounterDirective],
+    declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
     exports: [StoreComponent]
 })
 export class StoreModule {}
